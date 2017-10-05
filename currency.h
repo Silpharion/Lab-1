57ebb97aadcb2793ab79385d2092f3be07994586
Silpharion <xtarokundesu@gmail.com>
2017-10-04 21:48:06 -0700

    Add files via upload

---
+++ currency.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef CURRENCY_H
+#define CURRENCY_H
+#include <iostream>
+
+class Currency
+{
+	private: 
+		std::string currName;
+		std::string fracName;
+		int whParts;
+		double fracParts;
+	public:
+
+};
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "currency.h"
+using namespace std;
+
+int main()
+{
+
+}
